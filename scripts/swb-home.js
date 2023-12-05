43749fcf9c0d465f79df5a41728a894d9a3d4916
Programmac1 <appledan616@gmail.com>
2023-12-05 13:21:14 +1100

    reverted scrollbar js effect added
    it messed up the parallax effect- the 2 cant exist together unfortunately
    
    added chatGPT transcript to google drive

--- scripts/swb-home.js
+++ scripts/swb-home.js
@@ -1,7 +1,7 @@
 
 
 
-document.addEventListener('scroll', function () {
+/*document.addEventListener('scroll', function () {
     console.log('scroll event fired')
     const scrollPosition = window.scrollY;
     const navbar = document.querySelector('.navbar');
@@ -11,9 +11,11 @@ document.addEventListener('scroll', function () {
     } else {
         navbar.classList.remove('scrolled')
     }
-});
+});*/
 
 console.log('Document body height:', document.body.clientHeight);
 
-const videoBanner = document.querySelector('.video-banner');
+/*const videoBanner = document.querySelector('.video-banner');
 videoBanner.classList.toggle('force-redraw'); /*added this to fix video fitting vertical issue- did a browser redraw to fix rendering issue with blank space above video-banner*/
+
+//the above code regarding the scrollbar breaks the parallax effect// its because parallax messes with the scrolling height dimensions which need to be defined and used to make the scrollbar change class depending on the scrollY property
