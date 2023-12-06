51e2ba30b1ba650fd1eef8af7a09e6aa1100324e
Programmac1 <appledan616@gmail.com>
2023-12-06 18:05:51 +1100

    tried to get navbar to disappear as I scrolled
    currently unable to get it to work without breaking the parallax effect- changing the wrapper element to min-height:100vh makes the navbar js work but disables the parallax

--- scripts/swb-home.js
+++ scripts/swb-home.js
@@ -16,6 +16,21 @@
 console.log('Document body height:', document.body.clientHeight);
 
 /*const videoBanner = document.querySelector('.video-banner');
-videoBanner.classList.toggle('force-redraw'); /*added this to fix video fitting vertical issue- did a browser redraw to fix rendering issue with blank space above video-banner*/
+videoBanner.classList.toggle('force-redraw'); /*added this to fix video fitting vertical issue- did a browser redraw to fix rendering issue with blank space above video-banner after scrollbar code was added*/
 
 //the above code regarding the scrollbar breaks the parallax effect// its because parallax messes with the scrolling height dimensions which need to be defined and used to make the scrollbar change class depending on the scrollY property
+
+document.addEventListener("DOMContentLoaded", function () {
+    let prevScrollPosition = window.pageYOffset;
+    const navbar = document.querySelector('.navbar-wrap');
+
+window.onscroll = function () {
+    const currentScrollPosition = window.pageYOffset;
+    if(prevScrollPosition > currentScrollPosition){
+        navbar.style.top = "0";
+    }else{
+        navbar.style.top = "-50px";
+    }
+    prevScrollPosition = currentScrollPosition;
+}
+});
