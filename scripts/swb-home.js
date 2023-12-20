395083e964da57e847a26eff3d06666fdf0da5bd
Programmac1 <148036603+Programmac1@users.noreply.github.com>
2023-12-20 14:45:43 +1100

    made changes to video-banner
    made changes to video-banner css and javascript to make the homepage more compatible with mac

--- scripts/swb-home.js
+++ scripts/swb-home.js
@@ -20,7 +20,7 @@ videoBanner.classList.toggle('force-redraw'); /*added this to fix video fitting 
 
 //the above code regarding the scrollbar breaks the parallax effect// its because parallax messes with the scrolling height dimensions which need to be defined and used to make the scrollbar change class depending on the scrollY property
 
-document.addEventListener("DOMContentLoaded", function () {
+/*document.addEventListener("DOMContentLoaded", function () {
     let prevScrollPosition = window.pageYOffset;
     const navbar = document.querySelector('.navbar-wrap');
 
@@ -33,4 +33,11 @@ window.onscroll = function () {
     }
     prevScrollPosition = currentScrollPosition;
 }
-});
+});*/
+
+window.onload = function() {
+    var videoBanner = document.querySelector('.video-banner');
+    videoBanner.style.transform = 'translateZ(0)';
+};
+
+//this function fixed the initial fullscreen video-banner issue//
